ffa4c802b7101a1e85117b91594869c5ee701062
StevenmHernandez <contact@stevenhdesigns.com>
2016-03-16 22:16:20 -0400

    Project-4: added test for line format, moved items to separate methods

--- Project-4/PopcornEvaluation.java
+++ Project-4/PopcornEvaluation.java
@@ -7,43 +7,69 @@ public class PopcornEvaluation {
   final static int MIN_COL_LENGTH = 24;
   
   public static void main(String[] args) throws FileNotFoundException { 
-    // request filename
     Scanner in = new Scanner(System.in);
+    File file = getFile(in);
+    
+    printTableHeader();
+    
+    Scanner fileReader = new Scanner(file);
+    // foreach line
+    while (fileReader.hasNextLine()) {
+      String line = fileReader.nextLine();
+      if (testLineFormat(line)) {
+        printFormattedLine(parseFarmName(line), parsePintsPerAcre(line));
+      }
+    }
+  }
+  
+  public static File getFile(Scanner in) {
+    // request filename from user
     System.out.println("Input filename: ");
     String filename = in.next();
     
-    // load file
     File file = new File(filename);
-
     while (!file.exists()) {
       // re-request filename
       System.out.printf("Looks like the file `%s` doesn't exist.\n", filename);
-      System.out.println("Input another filename: ");
-      filename = in.next();
-      file = new File(filename);
+      return getFile(in);
     }
-
-    Scanner fileReader = new Scanner(file);
     
-    // table heading
+    return file;
+  }
+  
+  public static void printTableHeader() {
     System.out.println("                    Popcorn Co-op");
     System.out.println();
     System.out.println("                              Production in Hundreds");
     System.out.println("                              of Pint Jars per Acre");
     System.out.println("Farm Name                        1   2   3   4   5   6");
     System.out.println("                              ---|---|---|---|---|---|");
-    
-    // foreach line
-    while (fileReader.hasNextLine()) {
-      String line = fileReader.nextLine();
-      printFormatted(parseFarmName(line), parsePintsPerAcre(line));
-    }
   }
-  
+
+  public static boolean testLineFormat(String line) {
+    // check for comma
+    if (line.indexOf(',') <= 0) return false;
+    
+    // check that their is a String after the comma
+    if (line.indexOf(',') == line.length() - 1) return false;
+
+    Scanner scan = new Scanner(line.split(",")[1].trim());
+    // check to see that there is data for # of acres
+    if (!scan.hasNextDouble()) return false;
+    scan.nextDouble(); // acres
+
+    // check to see that there is data for # of pints
+    if (!scan.hasNextDouble()) return false;
+    scan.nextDouble();
+
+    // everything seems cool, let it go dude.
+    return true; // pints
+  }
+
   public static String parseFarmName(String line) {
     return line.split(",")[0].trim();
   }
-  
+
   public static double parsePintsPerAcre(String line) {
     Scanner scan = new Scanner(line.split(",")[1]);
     
@@ -51,8 +77,8 @@ public class PopcornEvaluation {
     double pints = scan.nextDouble();
     return pints / acres;
   }
-  
-  public static void printFormatted(String farmName, double pintsPerAcre) {
+
+  public static void printFormattedLine(String farmName, double pintsPerAcre) {
     System.out.print(farmName);
     // padding for name
     for (int i = 0; i < 30 - farmName.length(); i++) {
